69c1d8085ebf7a9a44c9ad95ad48dbf05dcba592
Llewellyn Falco <isidore@setgame.com>
2019-04-26 11:16:55 -0400

    "r clean ifs "

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -33,9 +33,6 @@ class GildedRose
       {
         if (item.quality > 0)
         {
-          if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
-          {
-          }
         }
       }
       return;
