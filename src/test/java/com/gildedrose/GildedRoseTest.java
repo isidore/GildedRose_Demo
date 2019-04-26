0f567ed958f321b1aeaba0256f9e2a7b7bb65b91
Llewellyn Falco <isidore@setgame.com>
2019-04-26 10:41:36 -0400

    "t add TCR"

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,9 +3,10 @@ package com.gildedrose;
 import java.text.MessageFormat;
 
 import org.approvaltests.Approvals;
+import org.approvaltests.testcommitrevert.TestCommitRevertTest;
 import org.junit.Test;
 
-public class GildedRoseTest
+public class GildedRoseTest extends TestCommitRevertTest
 {
   @Test
   public void foo()
