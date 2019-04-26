fc417b2c6c29f5f42b363b6c42038035aa325ee0
Llewellyn Falco <isidore@setgame.com>
2019-04-26 11:25:54 -0400

    "r extract default "

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,6 @@
 package com.gildedrose;
 
-class GildedRose extends UpdateSulfuras
+class GildedRose extends UpdateDefault
 {
   Item[] items;
   public GildedRose(Item[] items)
@@ -37,25 +37,5 @@ class GildedRose extends UpdateSulfuras
       updateDefaultItem(item);
       return;
     }
-  }
-  public boolean ifDefault()
-  {
-    return true;
-  }
-  public void updateDefaultItem(Item item)
-  {
-    if (item.quality > 0)
-    {
-      item.quality = item.quality - 1;
-    }
-    item.sellIn = item.sellIn - 1;
-    if (item.sellIn < 0)
-    {
-      if (item.quality > 0)
-      {
-        item.quality = item.quality - 1;
-      }
-    }
-    return;
   }
 }
---
+++ src/main/java/com/gildedrose/UpdateDefault.java
@@ -0,0 +1,31 @@
+package com.gildedrose;
+
+public class UpdateDefault extends UpdateSulfuras
+{
+  public UpdateDefault()
+  {
+    super();
+  }
+
+  public boolean ifDefault()
+  {
+    return true;
+  }
+
+  public void updateDefaultItem(Item item)
+  {
+    if (item.quality > 0)
+    {
+      item.quality = item.quality - 1;
+    }
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn < 0)
+    {
+      if (item.quality > 0)
+      {
+        item.quality = item.quality - 1;
+      }
+    }
+    return;
+  }
+}
