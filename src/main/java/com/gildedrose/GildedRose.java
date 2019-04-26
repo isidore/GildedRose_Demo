51178e305887f83cc5837424fc1be97957ccb1c1
Llewellyn Falco <isidore@setgame.com>
2019-04-26 11:05:14 -0400

    "r deleted red "

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -21,13 +21,6 @@ class GildedRose
     {
       if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
       {
-        if (item.quality > 0)
-        {
-          if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
-          {
-            item.quality = item.quality - 1;
-          }
-        }
       }
       else
       {
