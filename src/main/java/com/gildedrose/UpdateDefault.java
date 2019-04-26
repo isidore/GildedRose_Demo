23ac62a7931e3a7eb2cae3ff370c8e137c2cbc66
Llewellyn Falco <isidore@setgame.com>
2019-04-26 11:43:05 -0400

    "r no > "

--- src/main/java/com/gildedrose/UpdateDefault.java
+++ src/main/java/com/gildedrose/UpdateDefault.java
@@ -18,7 +18,7 @@ public class UpdateDefault implements ItemUpdater
       item.sellIn = item.sellIn - 1;
       if (item.sellIn < 0)
       {
-        if (item.quality > 0)
+        if (0 < item.quality)
         {
           item.quality = item.quality - 1;
         }
