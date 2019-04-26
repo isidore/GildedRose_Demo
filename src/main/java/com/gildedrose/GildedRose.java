f348aa2f670ecb15172517379353a40f1b2b1981
Llewellyn Falco <isidore@setgame.com>
2019-04-26 11:22:54 -0400

    "r dead code "

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -106,9 +106,6 @@ class GildedRose
     if (item.quality < 50)
     {
       item.quality = item.quality + 1;
-      if (ifBackstagePasses(item))
-      {
-      }
     }
     item.sellIn = item.sellIn - 1;
     if (item.sellIn < 0)
