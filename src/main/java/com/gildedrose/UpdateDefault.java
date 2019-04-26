1c48b6ad9fc06819cfe3aae1489d2e34e4ba1062
Llewellyn Falco <isidore@setgame.com>
2019-04-26 11:42:07 -0400

    "r clean if "

--- src/main/java/com/gildedrose/UpdateDefault.java
+++ src/main/java/com/gildedrose/UpdateDefault.java
@@ -14,10 +14,7 @@ public class UpdateDefault implements ItemUpdater
   {
     if (item.quality > 0)
     {
-      if (true)
-      {
-        item.quality = item.quality - 1;
-      }
+      item.quality = item.quality - 1;
       item.sellIn = item.sellIn - 1;
       if (item.sellIn < 0)
       {
