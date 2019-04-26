94c1122b1c4ea7074a400dde0877f50931de7e7d
Llewellyn Falco <isidore@setgame.com>
2019-04-26 11:19:59 -0400

    "r extract if clause "

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -17,7 +17,7 @@ class GildedRose
   }
   public void updateQuality(Item item)
   {
-    if (item.name.equals("Aged Brie"))
+    if (isBrie(item))
     {
       updateQualityForBrie(item);
       return;
@@ -37,6 +37,10 @@ class GildedRose
       updateDefaultItem(item);
       return;
     }
+  }
+  public boolean isBrie(Item item)
+  {
+    return item.name.equals("Aged Brie");
   }
   private void updateQualityForSulfuras(Item item)
   {
