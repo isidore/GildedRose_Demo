eef20db0925f704e166cd9120a69d87a937e456a
Llewellyn Falco <isidore@setgame.com>
2019-04-26 10:50:53 -0400

    "t 9 tests "

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -5,13 +5,13 @@ import java.text.MessageFormat;
 import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.Test;
 
-public class GildedRoseTest //extends FasterTestCommitRevertTest
+public class GildedRoseTest extends FasterTestCommitRevertTest
 {
   @Test
   public void foo() throws Exception
   {
     String[] names = {"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert"};
-    Integer qualities[] = {0};
+    Integer qualities[] = {-1, 0, 1};
     CombinationApprovals.verifyAllCombinations(this::doStuff, names, qualities);
   }
   public String doStuff(String name, Integer quality)
