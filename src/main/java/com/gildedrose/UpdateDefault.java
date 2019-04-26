1e894430d5c3c8d66bb78c9867b9eb89397a5b58
Llewellyn Falco <isidore@setgame.com>
2019-04-26 11:40:59 -0400

    "r inline "

--- src/main/java/com/gildedrose/UpdateDefault.java
+++ src/main/java/com/gildedrose/UpdateDefault.java
@@ -14,28 +14,35 @@ public class UpdateDefault implements ItemUpdater
   {
     if (item.quality > 0)
     {
-      stuff(item);
+      if (item.quality > 0)
+      {
+        item.quality = item.quality - 1;
+      }
+      item.sellIn = item.sellIn - 1;
+      if (item.sellIn < 0)
+      {
+        if (item.quality > 0)
+        {
+          item.quality = item.quality - 1;
+        }
+      }
       return;
     }
     else
-    {
-      stuff(item);
-      return;
-    }
-  }
-  public void stuff(Item item)
-  {
-    if (item.quality > 0)
-    {
-      item.quality = item.quality - 1;
-    }
-    item.sellIn = item.sellIn - 1;
-    if (item.sellIn < 0)
     {
       if (item.quality > 0)
       {
         item.quality = item.quality - 1;
       }
+      item.sellIn = item.sellIn - 1;
+      if (item.sellIn < 0)
+      {
+        if (item.quality > 0)
+        {
+          item.quality = item.quality - 1;
+        }
+      }
+      return;
     }
   }
 }
