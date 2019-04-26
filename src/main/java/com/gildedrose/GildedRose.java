a6c1625b734e1f1cc4270f95d9c8fee2d0bc07ab
Llewellyn Falco <isidore@setgame.com>
2019-04-26 11:13:40 -0400

    "r extract method "

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -24,29 +24,7 @@ class GildedRose
     }
     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
     {
-      if (item.quality < 50)
-      {
-        item.quality = item.quality + 1;
-        if (item.sellIn < 11)
-        {
-          if (item.quality < 50)
-          {
-            item.quality = item.quality + 1;
-          }
-        }
-        if (item.sellIn < 6)
-        {
-          if (item.quality < 50)
-          {
-            item.quality = item.quality + 1;
-          }
-        }
-      }
-      item.sellIn = item.sellIn - 1;
-      if (item.sellIn < 0)
-      {
-        item.quality = item.quality - item.quality;
-      }
+      updateQualityForBackstagePasses(item);
       return;
     }
     else
@@ -74,6 +52,33 @@ class GildedRose
       }
       return;
     }
+  }
+  public void updateQualityForBackstagePasses(Item item)
+  {
+    if (item.quality < 50)
+    {
+      item.quality = item.quality + 1;
+      if (item.sellIn < 11)
+      {
+        if (item.quality < 50)
+        {
+          item.quality = item.quality + 1;
+        }
+      }
+      if (item.sellIn < 6)
+      {
+        if (item.quality < 50)
+        {
+          item.quality = item.quality + 1;
+        }
+      }
+    }
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn < 0)
+    {
+      item.quality = item.quality - item.quality;
+    }
+    return;
   }
   public void updateQualityForBrie(Item item)
   {
