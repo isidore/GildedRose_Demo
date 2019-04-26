124ed95c2ee8e9731ec079463e350ef88d32bfb8
Llewellyn Falco <isidore@setgame.com>
2019-04-26 11:43:59 -0400

    "r moved sellin "

--- src/main/java/com/gildedrose/UpdateDefault.java
+++ src/main/java/com/gildedrose/UpdateDefault.java
@@ -12,10 +12,10 @@ public class UpdateDefault implements ItemUpdater
   }
   public void updateItem(Item item)
   {
+    item.sellIn = item.sellIn - 1;
     if (0 < item.quality)
     {
       item.quality = item.quality - 1;
-      item.sellIn = item.sellIn - 1;
       if (item.sellIn < 0)
       {
         if (0 < item.quality)
@@ -27,7 +27,6 @@ public class UpdateDefault implements ItemUpdater
     }
     else
     {
-      item.sellIn = item.sellIn - 1;
       return;
     }
   }
