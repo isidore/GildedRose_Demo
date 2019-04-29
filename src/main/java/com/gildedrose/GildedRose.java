e49dc43ed0c791162eb515d850df6ab5fd13a256
Llewellyn Falco <isidore@setgame.com>
2019-04-29 16:51:06 -0500

    "r delete yellow "

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -66,7 +66,7 @@ class GildedRose
       {
         if (item.quality > 0)
         {
-          if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
+          if (true)
           {
             item.quality = item.quality - 1;
           }
