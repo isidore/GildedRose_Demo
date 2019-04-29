2b420c504e4aef46924ccb4dc93cb6d29f42081f
Llewellyn Falco <isidore@setgame.com>
2019-04-29 16:52:14 -0500

    "r clean ifs "

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -35,10 +35,7 @@ class GildedRose
     {
       if (item.quality > 0)
       {
-        if (true)
-        {
-          item.quality = item.quality - 1;
-        }
+        item.quality = item.quality - 1;
       }
       if (true)
       {
