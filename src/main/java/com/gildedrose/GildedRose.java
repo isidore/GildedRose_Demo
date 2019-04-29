62f65ffce7a6f9a9d16dc1a9d2cdf1d023d666eb
Llewellyn Falco <isidore@setgame.com>
2019-04-29 16:52:34 -0500

    "r join "

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -38,12 +38,9 @@ class GildedRose
         item.quality = item.quality - 1;
       }
       item.sellIn = item.sellIn - 1;
-      if (item.sellIn < 0)
+      if (item.sellIn < 0 && item.quality > 0)
       {
-        if (item.quality > 0)
-        {
-          item.quality = item.quality - 1;
-        }
+        item.quality = item.quality - 1;
       }
       return;
     }
