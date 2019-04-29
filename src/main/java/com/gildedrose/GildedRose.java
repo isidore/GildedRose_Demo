72e926ecf8e444a4e7511a91ce13e2b9c3af4f69
Llewellyn Falco <isidore@setgame.com>
2019-04-29 16:52:56 -0500

    "r extract default "

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -33,17 +33,22 @@ class GildedRose
     }
     else
     {
-      if (item.quality > 0)
-      {
-        item.quality = item.quality - 1;
-      }
-      item.sellIn = item.sellIn - 1;
-      if (item.sellIn < 0 && item.quality > 0)
-      {
-        item.quality = item.quality - 1;
-      }
+      updateDefault(item);
       return;
     }
+  }
+  public void updateDefault(Item item)
+  {
+    if (item.quality > 0)
+    {
+      item.quality = item.quality - 1;
+    }
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn < 0 && item.quality > 0)
+    {
+      item.quality = item.quality - 1;
+    }
+    return;
   }
   public void updateBackstagePasses(Item item)
   {
