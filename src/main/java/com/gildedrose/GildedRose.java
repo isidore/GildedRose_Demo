38ac7227a621ea2dc748d955922f5b9537e536f8
Llewellyn Falco <isidore@setgame.com>
2019-05-09 14:57:15 +0200

    "r clean ifs "

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -24,27 +24,21 @@ class GildedRose
     }
     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
     {
-      if (false)
+      if (item.quality < 50)
       {
-      }
-      else
-      {
-        if (item.quality < 50)
+        item.quality = item.quality + 1;
+        if (item.sellIn < 11)
         {
-          item.quality = item.quality + 1;
-          if (item.sellIn < 11)
+          if (item.quality < 50)
           {
-            if (item.quality < 50)
-            {
-              item.quality = item.quality + 1;
-            }
+            item.quality = item.quality + 1;
           }
-          if (item.sellIn < 6)
+        }
+        if (item.sellIn < 6)
+        {
+          if (item.quality < 50)
           {
-            if (item.quality < 50)
-            {
-              item.quality = item.quality + 1;
-            }
+            item.quality = item.quality + 1;
           }
         }
       }
