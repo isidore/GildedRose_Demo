d3616d4237215e2ec126e36311a5f92bd3e9dd4a
Llewellyn Falco <isidore@setgame.com>
2019-05-09 15:02:17 +0200

    "r local "

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -39,16 +39,17 @@ class GildedRose
   }
   public void updateDefault(Item item)
   {
+    int degardRate = 1;
     if (item.quality > 0)
     {
-      item.quality = item.quality - 1;
+      item.quality = item.quality - degardRate;
     }
-    item.sellIn = item.sellIn - 1;
+    item.sellIn = item.sellIn - degardRate;
     if (item.sellIn < 0)
     {
       if (item.quality > 0)
       {
-        item.quality = item.quality - 1;
+        item.quality = item.quality - degardRate;
       }
     }
     return;
