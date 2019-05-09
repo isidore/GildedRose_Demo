44ed11748c5d56551cdabd1764c0b18651e62b40
Llewellyn Falco <isidore@setgame.com>
2019-05-09 14:47:18 +0200

    "r method "

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -16,6 +16,11 @@ class GildedRose {
 
     public void dostuff(Item item)
     {
+      doStuff2(item);
+    }
+
+    public void doStuff2(Item item)
+    {
       if (!item.name.equals("Aged Brie")
               && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
           if (item.quality > 0) {
