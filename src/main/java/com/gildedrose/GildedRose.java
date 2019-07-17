aefaded451dea7f9dbde3b3a352b75afe9993756
Llewellyn Falco <isidore@setgame.com>
2019-07-17 11:06:20 -0600

    r introduced if

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -22,6 +22,12 @@ class GildedRose
       stuffWithAgedBrie(item);
       return;
     }
+    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
+    {
+    }
+    else
+    {
+    }
     otherStuff(item);
     return;
   }
