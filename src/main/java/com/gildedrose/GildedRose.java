46819a7922a31637d5847a80d3e1033c33828c02
Llewellyn Falco <isidore@setgame.com>
2019-07-17 11:08:23 -0600

    r delete yellow

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -54,7 +54,7 @@ class GildedRose
       }
       if (item.sellIn < 0)
       {
-        if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
+        if (false)
         {
         }
         else
