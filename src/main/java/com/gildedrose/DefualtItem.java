f466a84e4d38588385cb660a31c3a221cdc5c7f5
Llewellyn Falco <isidore@setgame.com>
2019-07-17 11:22:34 -0600

    r extract superclass

---
+++ src/main/java/com/gildedrose/DefualtItem.java
@@ -0,0 +1,28 @@
+package com.gildedrose;
+
+public class DefualtItem extends Sulfuras
+{
+  public DefualtItem()
+  {
+    super();
+  }
+
+  protected boolean isDefault()
+  {
+    return true;
+  }
+
+  protected void doStuffForEverythingElse(Item item)
+  {
+    if (item.quality > 0)
+    {
+      item.quality = item.quality - 1;
+    }
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn < 0 && item.quality > 0)
+    {
+      item.quality = item.quality - 1;
+    }
+    return;
+  }
+}
--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,6 @@
 package com.gildedrose;
 
-class GildedRose extends Sulfuras
+class GildedRose extends DefualtItem
 {
   Item[] items;
   public GildedRose(Item[] items)
@@ -37,22 +37,5 @@ class GildedRose extends Sulfuras
       doStuffForEverythingElse(item);
       return;
     }
-  }
-  private boolean isDefault()
-  {
-    return true;
-  }
-  private void doStuffForEverythingElse(Item item)
-  {
-    if (item.quality > 0)
-    {
-      item.quality = item.quality - 1;
-    }
-    item.sellIn = item.sellIn - 1;
-    if (item.sellIn < 0 && item.quality > 0)
-    {
-      item.quality = item.quality - 1;
-    }
-    return;
   }
 }
