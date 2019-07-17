1825c208bc2ef307992e60ca27876b27ad7bbce1
Llewellyn Falco <isidore@setgame.com>
2019-07-17 11:11:16 -0600

    r intro if

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -27,6 +27,12 @@ class GildedRose
       doStuffForBackstagePasses(item);
       return;
     }
+    if (item.name.equals("Sulfuras, Hand of Ragnaros"))
+    {
+    }
+    else
+    {
+    }
     doOther(item);
     return;
   }
