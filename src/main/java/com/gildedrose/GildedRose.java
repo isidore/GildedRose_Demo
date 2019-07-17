ed044d1b63b313c03d599172dd3d3386d38b42c5
Llewellyn Falco <isidore@setgame.com>
2019-07-17 11:12:07 -0600

    r deleted red

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -37,7 +37,6 @@ class GildedRose
       }
       if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
       {
-        item.sellIn = item.sellIn - 1;
       }
       if (item.sellIn < 0 && item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros"))
       {
