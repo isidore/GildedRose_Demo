00a294e9ea0acc3a2dd45d4b750f515901c5e500
Llewellyn Falco <isidore@setgame.com>
2019-07-17 10:51:20 -0600

    t 336 tests

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,6 +1,7 @@
 package com.gildedrose;
 
 import org.approvaltests.combinations.CombinationApprovals;
+import org.approvaltests.legacycode.Range;
 import org.junit.Test;
 
 public class GildedRoseTest
@@ -13,7 +14,7 @@ public class GildedRoseTest
                       "Backstage passes to a TAFKAL80ETC concert",
                       "Sulfuras, Hand of Ragnaros"};
     Integer[] qualities = {0, -1, 1, 49, 50, 51};
-    Integer[] sellIns = {0};
+    Integer[] sellIns = Range.get(-1, 12);
     CombinationApprovals.verifyAllCombinations(this::doStuff, names, sellIns, qualities);
   }
   private String doStuff(String name, Integer sellIn, Integer quality)
