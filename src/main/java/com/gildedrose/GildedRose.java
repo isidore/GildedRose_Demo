91ab7ea32f2b71510586d8d01a980b10450ede76
Llewellyn Falco <isidore@setgame.com>
2019-07-17 10:57:59 -0600

    r introduced if

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,73 +1,96 @@
 package com.gildedrose;
 
-class GildedRose {
-    Item[] items;
-
-    public GildedRose(Item[] items) {
-        this.items = items;
+class GildedRose
+{
+  Item[] items;
+  public GildedRose(Item[] items)
+  {
+    this.items = items;
+  }
+  public void updateQuality()
+  {
+    for (int i = 0; i < items.length; i++)
+    {
+      Item item = items[i];
+      stuff(item);
     }
-
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            Item item = items[i];
-            stuff(item);
+  }
+  private void stuff(Item item)
+  {
+    if (item.name.equals("Aged Brie"))
+    {
+    }
+    else
+    {
+    }
+    stuff2(item);
+  }
+  private void stuff2(Item item)
+  {
+    if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
+    {
+      if (item.quality > 0)
+      {
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
+        {
+          item.quality = item.quality - 1;
         }
+      }
     }
-
-    private void stuff(Item item)
+    else
     {
-      stuff2(item);
-    }
-
-    private void stuff2(Item item)
-    {
-      if (!item.name.equals("Aged Brie")
-              && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-          if (item.quality > 0) {
-              if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                  item.quality = item.quality - 1;
-              }
-          }
-      } else {
-          if (item.quality < 50) {
+      if (item.quality < 50)
+      {
+        item.quality = item.quality + 1;
+        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
+        {
+          if (item.sellIn < 11)
+          {
+            if (item.quality < 50)
+            {
               item.quality = item.quality + 1;
-
-              if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                  if (item.sellIn < 11) {
-                      if (item.quality < 50) {
-                          item.quality = item.quality + 1;
-                      }
-                  }
-
-                  if (item.sellIn < 6) {
-                      if (item.quality < 50) {
-                          item.quality = item.quality + 1;
-                      }
-                  }
-              }
+            }
           }
-      }
-
-      if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-          item.sellIn = item.sellIn - 1;
-      }
-
-      if (item.sellIn < 0) {
-          if (!item.name.equals("Aged Brie")) {
-              if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                  if (item.quality > 0) {
-                      if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                          item.quality = item.quality - 1;
-                      }
-                  }
-              } else {
-                  item.quality = item.quality - item.quality;
-              }
-          } else {
-              if (item.quality < 50) {
-                  item.quality = item.quality + 1;
-              }
+          if (item.sellIn < 6)
+          {
+            if (item.quality < 50)
+            {
+              item.quality = item.quality + 1;
+            }
           }
+        }
       }
     }
+    if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
+    {
+      item.sellIn = item.sellIn - 1;
+    }
+    if (item.sellIn < 0)
+    {
+      if (!item.name.equals("Aged Brie"))
+      {
+        if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
+        {
+          if (item.quality > 0)
+          {
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
+            {
+              item.quality = item.quality - 1;
+            }
+          }
+        }
+        else
+        {
+          item.quality = item.quality - item.quality;
+        }
+      }
+      else
+      {
+        if (item.quality < 50)
+        {
+          item.quality = item.quality + 1;
+        }
+      }
+    }
+  }
 }
