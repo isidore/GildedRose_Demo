5335fa61d580b67d6223eb01ff5871b8e5e96ce0
Llewellyn Falco <isidore@setgame.com>
2019-07-17 11:25:43 -0600

    r seperate

--- src/main/java/com/gildedrose/Sulfuras.java
+++ src/main/java/com/gildedrose/Sulfuras.java
@@ -1,17 +1,15 @@
 package com.gildedrose;
 
-public class Sulfuras extends BackstagePasses
+public class Sulfuras
 {
   public Sulfuras()
   {
     super();
   }
-
   protected boolean isSulfuras(Item item)
   {
     return item.name.equals("Sulfuras, Hand of Ragnaros");
   }
-
   protected void doStuffForSulfuras(Item item)
   {
     // TODO Auto-generated method stub
