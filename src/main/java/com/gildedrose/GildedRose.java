0c1a2294d581d32c0fdf1bb566d7ab4defe1a93b
Llewellyn Falco <isidore@setgame.com>
2019-07-17 11:10:34 -0600

    r   Extract Method

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -24,29 +24,7 @@ class GildedRose
     }
     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
     {
-      if (item.quality < 50)
-      {
-        item.quality = item.quality + 1;
-        if (item.sellIn < 11)
-        {
-          if (item.quality < 50)
-          {
-            item.quality = item.quality + 1;
-          }
-        }
-        if (item.sellIn < 6)
-        {
-          if (item.quality < 50)
-          {
-            item.quality = item.quality + 1;
-          }
-        }
-      }
-      item.sellIn = item.sellIn - 1;
-      if (item.sellIn < 0)
-      {
-        item.quality = item.quality - item.quality;
-      }
+      doStuffForBackstagePasses(item);
       return;
     }
     else
@@ -68,6 +46,33 @@ class GildedRose
       }
       return;
     }
+  }
+  private void doStuffForBackstagePasses(Item item)
+  {
+    if (item.quality < 50)
+    {
+      item.quality = item.quality + 1;
+      if (item.sellIn < 11)
+      {
+        if (item.quality < 50)
+        {
+          item.quality = item.quality + 1;
+        }
+      }
+      if (item.sellIn < 6)
+      {
+        if (item.quality < 50)
+        {
+          item.quality = item.quality + 1;
+        }
+      }
+    }
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn < 0)
+    {
+      item.quality = item.quality - item.quality;
+    }
+    return;
   }
   private void stuffWithAgedBrie(Item item)
   {
