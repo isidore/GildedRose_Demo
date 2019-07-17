8b70b9c3910e64248229d5bdf2c3380a088deb23
Llewellyn Falco <isidore@setgame.com>
2019-07-17 11:41:54 -0600

    r   Extract Variable

--- src/main/java/com/gildedrose/DefualtItem.java
+++ src/main/java/com/gildedrose/DefualtItem.java
@@ -14,14 +14,15 @@ public class DefualtItem implements ItemUpdater
   @Override
   public void updateQuality(Item item)
   {
+    int qualityRate = 1;
     if (item.quality > 0)
     {
-      item.quality = item.quality - 1;
+      item.quality = item.quality - qualityRate;
     }
     item.sellIn = item.sellIn - 1;
     if (item.sellIn < 0 && item.quality > 0)
     {
-      item.quality = item.quality - 1;
+      item.quality = item.quality - qualityRate;
     }
     return;
   }
