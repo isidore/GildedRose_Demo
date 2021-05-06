7956e5b89201d92e990e28eb02309d618ec8589f
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-05-06 11:53:20 -0600

    t extract method

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -15,11 +15,16 @@ public class GildedRoseTest {
     @Test
     public void foo() {
         String name = "foo";
+        String result = doStuff(name);
+        Approvals.verify(result);
+    }
+
+    private String doStuff(String name) {
         Item[] items = new Item[]{new Item(name, 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         String result = app.items[0].toString();
-        Approvals.verify(result);
+        return result;
     }
 
 }
