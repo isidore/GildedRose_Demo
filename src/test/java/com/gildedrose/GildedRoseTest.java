de68bc76762d7422446bb17f99bb9585f7e6d1bc
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-05-06 11:57:54 -0600

    t introduce parameter

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,7 +1,6 @@
 package com.gildedrose;
 
 import com.github.larseckart.tcr.FastTestCommitRevertMainExtension;
-import org.approvaltests.Approvals;
 import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,11 +15,11 @@ public class GildedRoseTest {
     @Test
     public void foo() {
         String names[] = {"foo","Aged Brie","Backstage passes to a TAFKAL80ETC concert"};
-        CombinationApprovals.verifyAllCombinations(this::doStuff, names);
+        CombinationApprovals.verifyAllCombinations(name -> doStuff(name, 0), names);
     }
 
-    private String doStuff(String name) {
-        Item[] items = new Item[]{new Item(name, 0, 0)};
+    private String doStuff(String name, Integer quality) {
+        Item[] items = new Item[]{new Item(name, 0, quality)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         String result = app.items[0].toString();
