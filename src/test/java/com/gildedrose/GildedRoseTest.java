0510fb0e09ca35654c5ce646cfe82460d7856232
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-05-06 11:28:23 -0600

    starting spot

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,16 +1,14 @@
 package com.gildedrose;
 
-import com.github.larseckart.tcr.TestCommitRevertMainExtension;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
- public class GildedRoseTest {
+public class GildedRoseTest {
 
     @Test
     public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("fix-me", app.items[0].name);
