4404ac7ba9a470bada9983ecb90213de64e5105f
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-05-06 11:52:45 -0600

    t extract variable

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,10 +14,12 @@ public class GildedRoseTest {
 
     @Test
     public void foo() {
-        Item[] items = new Item[]{new Item("foo", 0, 0)};
+        String name = "foo";
+        Item[] items = new Item[]{new Item(name, 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        Approvals.verify(app.items[0]);
+        Item result = app.items[0];
+        Approvals.verify(result);
     }
 
 }
