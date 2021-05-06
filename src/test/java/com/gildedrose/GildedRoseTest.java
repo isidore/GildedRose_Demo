46f377986300a94646e9f2356874561ccc9bd47e
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-05-06 12:04:43 -0600

    sellins

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,11 +14,12 @@ public class GildedRoseTest {
     public void foo() {
         String names[] = {"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert","Sulfuras, Hand of Ragnaros"};
         Integer qualities[] = {0,-1,1,49,50,51};
-        CombinationApprovals.verifyAllCombinations(this::doStuff, names, qualities);
+        Integer[] sellIns = {0};
+        CombinationApprovals.verifyAllCombinations(this::doStuff, names, sellIns, qualities);
     }
 
-    private String doStuff(String name, Integer quality) {
-        Item[] items = new Item[]{new Item(name, 0, quality)};
+    private String doStuff(String name, Integer sellIn, Integer quality) {
+        Item[] items = new Item[]{new Item(name, sellIn, quality)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         String result = app.items[0].toString();
