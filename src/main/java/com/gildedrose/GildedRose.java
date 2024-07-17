7e3dd8c2333159dee4ff37745c5ec710388d3ac1
Llewellyn Falco <llewellyn.falco@gmail.com>
2024-07-17 09:27:37 -0600

    - r false

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -11,8 +11,7 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
             Item item = items[i];
             if (item.name.equals("Aged Brie")) {
-                if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (false) {
 
                 } else {
                     if (item.quality < 50) {
