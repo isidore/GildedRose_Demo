36eff968c4c65731bd7b18fd2f4c56b346bef1d3
Llewellyn Falco <llewellyn.falco@gmail.com>
2024-07-17 08:57:51 -0600

    . r Extract Method

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,10 +14,15 @@ public class GildedRoseTest {
     @Test
     public void foo() {
         String name = "foo";
+        GildedRose app = doStuff(name);
+        Approvals.verify( app.items[0]);
+    }
+
+    private static GildedRose doStuff(String name) {
         Item[] items = new Item[]{new Item(name, 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        Approvals.verify( app.items[0]);
+        return app;
     }
 
 }
