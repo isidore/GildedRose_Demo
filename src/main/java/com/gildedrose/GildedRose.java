cf0500fa73c482576c99f49258cdc503c69224f8
Llewellyn Falco <llewellyn.falco@gmail.com>
2024-07-17 09:38:05 -0600

    . r extract method

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -12,38 +12,7 @@ class GildedRose {
             Item item = items[i];
             if (item.name.equals("Aged Brie")) updateQualityForAgedBrie(item);
             else {
-                if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
-
-
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-
-                            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                                if (item.sellIn < 11) {
-                                    if (item.quality < 50) {
-                                        item.quality = item.quality + 1;
-                                    }
-                                }
-
-                                if (item.sellIn < 6) {
-                                    if (item.quality < 50) {
-                                        item.quality = item.quality + 1;
-                                    }
-                                }
-                            }
-                        
-                    }
-
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.sellIn = item.sellIn - 1;
-                    }
-
-                    if (item.sellIn < 0) {
-
-                            item.quality = item.quality - item.quality;
-
-                    }
-                }
+                if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")) updateQualityForBackstagePasses(item);
                 else {
                     if (!item.name.equals("Aged Brie")
                         && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
@@ -75,6 +44,37 @@ class GildedRose {
                 }
 
             }
+        }
+    }
+
+    private static void updateQualityForBackstagePasses(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+
+            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+            }
+
+    }
+
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.sellIn = item.sellIn - 1;
+        }
+
+        if (item.sellIn < 0) {
+
+                item.quality = item.quality - item.quality;
+
         }
     }
 
