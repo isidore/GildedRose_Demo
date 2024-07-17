e0cae087680dfca1b6f3d532a9721db011ff4815
Llewellyn Falco <llewellyn.falco@gmail.com>
2024-07-17 09:26:34 -0600

    - r delete red

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -48,13 +48,6 @@ class GildedRose {
 
                 if (item.sellIn < 0) {
                     if (!item.name.equals("Aged Brie")) {
-                        if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (item.quality > 0) {
-                                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                    item.quality = item.quality - 1;
-                                }
-                            }
-                        } 
                     } else {
                         if (item.quality < 50) {
                             item.quality = item.quality + 1;
