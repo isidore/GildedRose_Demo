5c5b748c186b0183bbcf9015f44d99781bf2ce26
Llewellyn Falco <llewellyn.falco@gmail.com>
2024-07-17 09:36:57 -0600

    - r delete red

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -50,15 +50,9 @@ class GildedRose {
                     }
 
                     if (item.sellIn < 0) {
-                        if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (item.quality > 0) {
-                                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                    item.quality = item.quality - 1;
-                                }
-                            }
-                        } else {
+
                             item.quality = item.quality - item.quality;
-                        }
+                       
                     }
                 }
                 else {
@@ -73,7 +67,7 @@ class GildedRose {
                             }
                         }
 
-                    } 
+                    }
 
                     if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                         item.sellIn = item.sellIn - 1;
