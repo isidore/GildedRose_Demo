4fec316c48fb937b0a06941422cb4150de4e0103
Llewellyn Falco <llewellyn.falco@gmail.com>
2024-07-17 09:06:02 -0600

    . t 3

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,7 +14,7 @@ public class GildedRoseTest {
 
     @Test
     public void foo() {
-        String[] names = {"foo", "Aged Brie"};
+        String[] names = {"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert"};
         CombinationApprovals.verifyAllCombinations(GildedRoseTest::doStuff, names);
     }
 
