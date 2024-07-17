629197490cd6a95917672eede02fa3aa6449ca45
Llewellyn Falco <llewellyn.falco@gmail.com>
2024-07-17 09:26:20 -0600

    - r delete red

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -54,9 +54,7 @@ class GildedRose {
                                     item.quality = item.quality - 1;
                                 }
                             }
-                        } else {
-                            item.quality = item.quality - item.quality;
-                        }
+                        } 
                     } else {
                         if (item.quality < 50) {
                             item.quality = item.quality + 1;
